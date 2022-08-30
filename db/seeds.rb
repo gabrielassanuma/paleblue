a1207c86c1b163008315922fceac88c738b5a224
Diogo Macedo <dcapelamacedo@gmail.com>
2022-08-30 16:10:52 +0100

    seeding mistake

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,114 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+require 'securerandom'
+
+puts 'Starting Seeds...'
+puts 'Destroying users table'
+User.destroy_all
+puts 'Destroying token table'
+Token.destroy_all
+puts 'Destroying tk_balance'
+TkBalance.destroy_all
+puts 'Destroying transactions'
+Transaction.destroy_all
+
+puts "Create users..."
+2.times do
+  user = User.new(
+    wlt_address: SecureRandom.hex(10),
+    nickname: Faker::Cannabis.cannabinoid_abbreviation
+  )
+  user.save!
+end
+puts "Users done"
+
+users = User.all
+
+unlimited = [
+  'True',
+  'True',
+  'False',
+  'False',
+  'True'
+]
+
+max_mintable = [
+  nil,
+  nil,
+  10_000_000,
+  20_000_000,
+  nil
+]
+
+puts 'Create creator'
+creator = Creator.new(
+  q1: Faker::Lorem.paragraphs(number: 1),
+  q2: Faker::Lorem.paragraphs(number: 1),
+  q3: Faker::Lorem.paragraphs(number: 1),
+  non_profit: [true, false].sample,
+  about: Faker::Lorem.paragraphs(number: 1),
+  location: Faker::Address.city,
+  facebook: nil,
+  instagram: nil,
+  linkedin: nil,
+  website: nil,
+  tag1: Faker::Food.dish,
+  tag2: Faker::Food.dish,
+  tag3: Faker::Food.dish,
+  token: Token.first
+)
+creator.save!
+puts 'creator done'
+
+puts 'Create tokens...'
+5.times do |index|
+  token = Token.new(
+    tk_address: SecureRandom.hex,
+    unlimited: unlimited[index],
+    max_mint: max_mintable[index],
+    minted_so_far: rand(1...5_000_000),
+    user: users.first
+  )
+  token.save!
+end
+puts 'tokens done'
+
+tokens = Token.all
+
+puts 'Create tk_balance...'
+2.times do
+  tk_balance = TkBalance.new(
+    tk_amount: rand(1...5_000_000),
+    token: Token.first,
+    user: users.sample
+  )
+  tk_balance.save!
+end
+puts 'tk_balance done'
+
+puts 'Create transactions...'
+2.times do
+  transaction = Transaction.new(
+    tk_amount: rand(1...5_000_000),
+    from_user: User.first,
+    to_user: User.second,
+    token: Token.first
+  )
+  transaction.save!
+end
+puts 'transactions done'
+
+puts 'Create raffle'
+raffle = Raffle.new(
+  name: Faker::Science.element,
+  about: Faker::Lorem.paragraphs(number: 1),
+  tag1: Faker::Food.dish,
+  tag2: Faker::Food.dish,
+  tag3: Faker::Food.dish,
+  creator: Creator.first,
+  token: Token.first
+)
+raffle.save!
+puts 'raffle done'
