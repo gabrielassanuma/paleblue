3b77fb391c5faac924008f4b3d9896610daac63e
Diogo Macedo <dcapelamacedo@gmail.com>
2022-08-31 16:58:05 +0100

    transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,10 +1,32 @@
 class TransactionsController < ApplicationController
+
+  def show
+    @token = Transaction.find(params[:id])
+  end
+
   def new
+    @transaction = Transaction.new
+    @creator = Creator.find(params[:creator_id])
+    @tokens = Token.all
   end
 
   def create
+    @transaction = Transaction.new(transaction_params)
+    @transaction.from_user = current_user
+    creator = Creator.find(params[:creator_id])
+    @transaction.to_user = creator.token.user
+    @transaction.token = Token.find_by(nickname: params[:token])
+    if @transaction.save
+      redirect_to transaction_path(@transaction)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def show
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:tk_amount)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :creators, only: ['new', 'create', 'show', 'edit']
+  resources :creators, only: ['new', 'create', 'show', 'edit'] do
+    resources :transactions, only: ['new', 'create']
+  end
   get 'files/new', to: 'creators#files_new'
   get 'files/create', to: 'creators#files_create'
-  resources :transactions, only: ['new', 'create', 'index', 'show']
+  resources :transactions, only: ['index', 'show']
   resources :tokens, only: ['new', 'create', 'show']
   resources :raffles, only: ['new', 'create', 'show']
 end
