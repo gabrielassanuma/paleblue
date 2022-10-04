9b50f97d4385a44d1ac5b63c14f7c7753d30b74b
Brian Allemeier <brian.all09@protonmail.com>
2022-10-04 17:43:17 -0400

    update for construction

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :construction ]
 
   def home
+  end
+
+  def construction
   end
 
   def account
